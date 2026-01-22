aa0b9be6af3f29773427ab7c3d3dfdcee3f65492
Enrico <enrico.milanese@whoi.edu>
2026-01-22 17:09:49 -0500

    earthaccess already skips existing files

--- earthdataDownloader/main.py
+++ earthdataDownloader/main.py
@@ -2,39 +2,14 @@
 
 import earthaccess
 import glob
-from pathlib import Path
 
-def update_dl_list(results_list, dl_dir):
-
-    existing_list = glob.glob(dl_dir)
-
-    def extract_fname(fpath):
-        return fpath.rstrip('/').rsplit('/', 1)[-1]
-
-    results_fnames = [
-        extract_fname(result.data_links()[0]) for result in results_list
-    ]
-    existing_fnames = [
-        extract_fname(existing_file) for existing_file in existing_list
-    ]
-
-    # Get files not already present in destination folder
-    updated_list = [
-        fpath for fname, fpath in zip(results_fnames, results_list)
-        if fname not in existing_fnames
-    ] 
-
-    if len(updated_list)==0:
-        return False
-    else:
-        return updated_list
 
 def main():
 
     L3 = "AVHRR_SST_METOP_B_GLB-OSISAF-L3C-v1.0"
     L4 = "OSTIA-UKMO-L4-GLOB-REP-v2.0"
     bb = (27, -80, 53, -30)
-    time_range = ("2020-01-01", "2020-01-03")
+    time_range = ("2019-12-31", "2020-01-03")
     dl_folders = {}
     dl_folders[L3] = "./downloads/" + L3 + "/"
     dl_folders[L4] = "./downloads/" + L4 + "/"
@@ -57,20 +32,17 @@ def main():
 
         # 3. Download
         dl_dir = dl_folders[name]
-        to_download = update_dl_list(results, dl_dir)
-        dl_dir = Path(dl_dir)
+        print(
+            f"Found {len(results)} files."
+        )
         print(
             f"Downloading {name} data to {dl_dir}..."
         )
-        if not to_download:
-            print("No new file to download, exiting...")
-        else:
-            files = earthaccess.download(
-                to_download,
-                local_path = dl_dir,
-                show_progress = True
-            )
-
+        files = earthaccess.download(
+            results,
+            local_path = dl_dir,
+            show_progress = True
+        )
         print("Done.")
         
     return
